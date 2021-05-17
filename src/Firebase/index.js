ea18ce91f02fe7a691c42cd6eb9b9d78c3c31ad4
Pratik <shivam3448@gmail.com>
2021-05-17 21:42:07 +0530

    Added firebase and setup user login

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,15 +1,9 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCByRv3e82BQSh9mBnDoDursEIOTvtSHiw",
-  authDomain: "bold-streamer-188719.firebaseapp.com",
-  databaseURL: "https://bold-streamer-188719.firebaseio.com",
-  projectId: "bold-streamer-188719",
-  storageBucket: "bold-streamer-188719.appspot.com",
-  messagingSenderId: "56109275176",
-  appId: "1:56109275176:web:df998a5e21e3635b5a7d1d",
-};
+const firebaseConfig = {};
 
 const fire = firebase.initializeApp(firebaseConfig);
+
+// export default fire;
 
 export default fire;
